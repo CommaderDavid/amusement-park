8855c1a2737f3b0b2387df6c3a6111a775f66ec5
David Boedigheimer <stealth259@gmail.com>
2019-10-02 15:43:21 -0700

    Add color change based on height inputed.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   var age = parseInt(prompt("How tall are you?"));
 
   if (age >= 4) {
-    $('.over-4f').show();
+    $('.over-4f').addClass("red-background");
   } else if (age < 4) {
-    $('.under-4f').show();
+    $('.under-4f').addClass("green-background");
   }
 });
